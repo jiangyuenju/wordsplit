7eba2a51069859ffe8e9cc1820ec7612c71bba8e
jiangyuenju <tracy1995house@163.com>
2017-10-23 20:44:16 +0800

    Add files via upload

--- split.py
+++ split.py
@@ -4,11 +4,23 @@ import math
 
 def wordsplit(mydict, f, s):
     a1 = fmm(mydict, s)
-    a2 = rmm(mydict, s)
+    reverse_a2 = rmm(mydict, s)
+    a2 = []
+    for i in xrange(len(reverse_a2) - 1, -1, -1):
+        a2.append(reverse_a2[i])
+
+    #printarray(a1, a2, 1)
+    #printarray(a1, a2, 2)
+
     length = min(len(a1), len(a2))
-    a = a1
-    for i in xrange(length):
-        if a1[i] != a2[i]:
+    a = []
+    i = 0
+    index = 1
+    while i < length:
+        if a1[i] == a2[i]:
+            a.append(a1[i])
+            i += 1
+        else:
             last1 = last2 = i+3
             if (i+3) > len(a1):
                 last1 = len(a1)
@@ -17,9 +29,22 @@ def wordsplit(mydict, f, s):
             chunk1 = a1[i:last1]
             chunk2 = a2[i:last2]
             index = choosechunk(chunk1, chunk2, f)
-            break
+            if index == 1:
+                a.extend(a1[i:last1])
+                i = last1
+            elif index == 2:
+                a.extend(a2[i:last2])
+                i = last2
+            #i = i+3
 
-    printarray(a1, a2, index)
+    if i < len(a1) and index == 1:
+        a.extend(a1[i:])
+    if i < len(a2) and index == 2:
+        a.extend(a2[i:])
+
+    for i in xrange(len(a)):
+        print a[i] + " ",
+    print ""
 
 
 def printarray(a1, a2, type):
@@ -28,7 +53,7 @@ def printarray(a1, a2, type):
             print a1[i]+" ",
         print ""
     else:
-        for i in xrange(len(a2)-1, -1, -1):
+        for i in xrange(len(a2)):
             print a2[i]+" ",
         print ""
 
@@ -41,16 +66,16 @@ def choosechunk(c1, c2, f):
         total_l1 += len(c1[i])
     for i in xrange(len(c2)):
         total_l2 += len(c2[i])
-    print("total length1: %d, total length2: %d" % (total_l1, total_l2))
+    #print("total length1: %d, total length2: %d" % (total_l1, total_l2))
     if total_l1 > total_l2:
         return 1
     elif total_l1 < total_l2:
         return 2
 
     # largest average word length
-    average_l1 = total_l1 / len(c1)
-    average_l2 = total_l2 / len(c2)
-    print("average length1: %d, average length2: %d" % (average_l1, average_l2))
+    average_l1 = float(total_l1) / len(c1)
+    average_l2 = float(total_l2) / len(c2)
+    #print("average length1: %f, average length2: %f" % (average_l1, average_l2))
     if average_l1 > average_l2:
         return 1
     elif average_l1 < average_l2:
@@ -65,7 +90,7 @@ def choosechunk(c1, c2, f):
     for i in xrange(len(c2)):
         var_2 += (len(c2[i]) - average_l2) ** 2
     var_2 = var_2 / len(c2)
-    print("var1: %f, var2: %f" % (var_1, var_2))
+    #print("var1: %f, var2: %f" % (var_1, var_2))
     if var_1 < var_2:
         return 1
     elif var_1 > var_2:
@@ -79,7 +104,7 @@ def choosechunk(c1, c2, f):
     for i in xrange(len(c2)):
         if len(c2[i]) == 1:
             log2 += math.log(float(f[c2[i]]))
-    print("log1: %f, log2: %f" % (log1, log2))
+    #print("log1: %f, log2: %f" % (log1, log2))
     if log1 >= log2:
         return 1
     else:
@@ -101,6 +126,7 @@ def fmm(mydict, s):
 
 def rmm(mydict, s):
     splitarray = []
+
     for i in xrange(len(s)):
         if searchword(mydict, s[i:]):
             splitarray.append(s[i:])
